000d7952faf80104e5581b2e8fdda77a503cb79f
Haruya Kanamitsu <hkanamit@c6r2s12.42tokyo.jp>
2026-06-10 15:23:06 +0900

    ok

--- ex5/ft_data_stream.py
+++ ex5/ft_data_stream.py
@@ -4,35 +4,41 @@ import random
 import typing
 
 
-def gen_event() -> tuple[str, str]:
+def gen_event() -> typing.Generator[tuple[str, str], None, None]:
     name_list: list[str] = ["alice", "bob", "charlie", "dylan"]
     action_list: list[str] = ["run", "eat", "sleep", "grab", "move", "swim"]
-    name = random.choice(name_list)
-    action = random.choice(action_list)
-    return name, action
+    while True:
+        name = random.choice(name_list)
+        action = random.choice(action_list)
+        yield name, action
 
 
 def consume_event(
         tuple_list: list[tuple[str, str]]
-        ) -> typing.Generator[tuple[str, str], None, None]:
+        ) -> typing.Generator[tuple[str, str], list[tuple[str, str]], None]:
     while tuple_list:
-        yield tuple_list.pop(random.randint(0, len(tuple_list) - 1))
+        index: int = random.randint(0, len(tuple_list) - 1)
+        yield tuple_list.pop(index)
 
 
 def main() -> None:
     print("=== Game Data Stream Processor ===")
+
+    gen = gen_event()
     for i in range(0, 1000):
-        event = gen_event()
+        event = next(gen)
         print(f"Event {i}: Player {event[0]} did action {event[1]}")
 
     tuple_list: list[tuple[str, str]] = []
 
+    gen = gen_event()
     for i in range(0, 10):
-        tuple_list.append(gen_event())
+        tuple_list.append(next(gen))
     print(f"Built list of 10 events: {tuple_list}")
 
-    for event in consume_event(tuple_list):
-        print(f"Got event from list: {event}")
+    consume = consume_event(tuple_list)
+    for event_to_be_consumed in consume:
+        print(f"Got event from list: {event_to_be_consumed}")
         print(f"Remains in list: {tuple_list}")
 
 
